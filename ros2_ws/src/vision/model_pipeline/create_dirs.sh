dc25e7f64e40da66ebbdcf73471dd6bf9b335d02
Arthur Guihaire <arthur.guihaire@gmail.com>
2025-10-25 21:01:50 -0400

    remove duplicate datasets

---
+++ ros2_ws/src/vision/model_pipeline/create_dirs.sh
@@ -0,0 +1,17 @@
+mkdir data
+mkdir data/augmented
+mkdir data/augmented/images
+mkdir data/augmented/labels
+mkdir data/augmented/train
+mkdir data/augmented/test
+mkdir data/augmented/val
+mkdir data/augmented/train/images
+mkdir data/augmented/test/images
+mkdir data/augmented/val/images
+mkdir data/augmented/train/labels
+mkdir data/augmented/test/labels
+mkdir data/augmented/val/labels
+mkdir data/raw
+mkdir data/raw/images
+mkdir data/raw/labels
+
--- ros2_ws/src/vision/model_pipeline/data/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Douglas Vision Model > 2025-10-19 8:46pm
-https://universe.roboflow.com/robosub-2025-mu9e7/douglas-vision-model
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
