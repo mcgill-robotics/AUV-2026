bf104134d654cee030649a9eb8d3bc2c8d10a9c8
WilliamZhang2205 <williamzhang2205@gmail.com>
2026-01-10 15:44:56 -0500

    Corrected sensors launch file by making it more clean and organized with the new nodes. JP approved commit message uwu

--- ros2_ws/src/sensors/launch/sensor_node.launch.py
+++ ros2_ws/src/sensors/launch/sensor_node.launch.py
@@ -1,32 +1,52 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import UnlessCondition
-from launch.substitutions import LaunchConfiguration
+from launch.conditions import UnlessCondition, IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
 
 def generate_launch_description():
     # Set Sim condition
-    sim_condition = DeclareLaunchArgument(
-            'sim', default_value='false', description='no launch if in sim')
-    
+    sim_condition = DeclareLaunchArgument('sim', default_value='false', description='no launch if in sim')
     # Find other launch files to launch
-    included_launch = IncludeLaunchDescription(
+    launch_Xsens_Driver = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(['/root/AUV-2026/ros2_ws/install/xsens_mti_ros2_driver/share/xsens_mti_ros2_driver/launch', '/xsens_mti_node.launch.py']),
             condition=UnlessCondition(LaunchConfiguration('sim')))
-    
+   
+    # Serial connection group 
+    serial_group = GroupAction(
+            condition=UnlessCondition(LaunchConfiguration('sim')),
+            actions=[
+                Node(
+                    package='micro_ros_agent',
+                    executable='micro_ros_agent',
+                    name='display_micro_ros_agent',
+                    output='screen',
+                    arguments=['serial', '--dev', '/dev/display', '--baud-rate', '115200'],
+                ),
+            ]
+     )
+
+    sensors_node = GroupAction(
+            condition=UnlessCondition(LaunchConfiguration('sim')),
+            actions=[
+                Node(
+                    package='sensors',
+                    executable='sensor_node',
+                    name='sensor_node'
+                    )
+                ]
+            )
     # Get the sim parameter value
     sim = LaunchConfiguration('sim')
 
     # Launch :D
     return LaunchDescription([
        sim_condition,
-       included_launch,
-       Node(
-            package='sensors',
-            executable='sensor_node',
-            name='sensor_node'
-            )
+       sensors_node,
+       serial_group,
+       launch_Xsens_Driver
         ])
     
