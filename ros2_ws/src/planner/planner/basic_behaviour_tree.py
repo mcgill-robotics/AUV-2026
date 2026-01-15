51228da151574e0eac93432a3ea6d6eeb74c8625
William Zhang <williamzhang2205@gmail.com>
2026-01-14 22:08:20 -0500

    Created planner package and left a pytrees doc example for a syntax example, TODO: incorporate with ROS2 topics and subscribers and interrupts between leafs

---
+++ ros2_ws/src/planner/planner/basic_behaviour_tree.py
@@ -0,0 +1,32 @@
+import py_trees
+import py_trees_ros
+
+def main():
+    root = py_trees.composites.Selector("Selector", memory=False)
+    high = py_trees.behaviours.Success(name="High Priority")
+    med = py_trees.behaviours.Success(name="Med Priority")
+    low = py_trees.behaviours.Success(name="Low Priority")
+    root.add_children([high, med, low])
+
+    behaviour_tree = py_trees.trees.BehaviourTree(root=root)
+    print(py_trees.display.unicode_tree(root=root))
+    behaviour_tree.setup(timeout=15)
+
+
+    def print_tree(tree: py_trees.trees.BehaviourTree) -> None:
+
+        """Print the behaviour tree and its current status."""
+        print(py_trees.display.unicode_tree(root=tree.root, show_status=True))
+
+    try:
+        behaviour_tree.tick_tock(
+            period_ms=500,
+            number_of_iterations=py_trees.trees.CONTINUOUS_TICK_TOCK,
+            pre_tick_handler=None,
+            post_tick_handler=print_tree,
+        )
+    except KeyboardInterrupt:
+        behaviour_tree.interrupt()
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_ws/src/planner/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'planner'
+
+setup(
+    name="planner",
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + "planner"]),
+        ('share/' + "planner" , ['package.xml']),
+    ],
+    install_requires=['setuptools', 
+					  'py_trees',
+					  'py_trees_ros'],
+    zip_safe=True,
+    maintainer='willz',
+    maintainer_email='williamzhang2205@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'basic_behaviour_tree = planner.basic_behaviour_tree:main'
+        ],
+    },
+)
