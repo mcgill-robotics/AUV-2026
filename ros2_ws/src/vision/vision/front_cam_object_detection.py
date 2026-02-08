d92919618aecb6dfd0f1cb753fd74707d95aa112
Sohaib Kaidali <kaidalisohaib@gmail.com>
2026-02-08 12:04:36 -0500

    Refactor object_detection with Detection2DArray + some +x perm missing

--- ros2_ws/src/vision/vision/front_cam_object_detection.py
+++ ros2_ws/src/vision/vision/front_cam_object_detection.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 
@@ -8,7 +9,7 @@ from cv_bridge import CvBridge
 from ultralytics import YOLO
 
 from sensor_msgs.msg import Image
-from auv_msgs.msg import VisionObject, VisionObjectArray
+from vision_msgs.msg import Detection2DArray, Detection2D, ObjectHypothesisWithPose, BoundingBox2D
 
 
 class FrontCamDetectorNode(Node):
@@ -16,16 +17,16 @@ class FrontCamDetectorNode(Node):
         super().__init__('front_cam_object_detection')
 
         self.declare_parameter('class_names')
-        self.declare_parameter('frontcam_model_path')
-        self.declare_parameter('frontcam_input_topic')
-        self.declare_parameter('frontcam_output_topic')
+        self.declare_parameter('model_path')
+        self.declare_parameter('input_topic')
+        self.declare_parameter('output_topic')
         self.declare_parameter('queue_size')
  
 
         self.class_names = self.get_parameter('class_names').get_parameter_value().string_array_value
-        model_path = self.get_parameter('frontcam_model_path').get_parameter_value().string_value
-        input_topic = self.get_parameter('frontcam_input_topic').get_parameter_value().string_value
-        output_topic = self.get_parameter('frontcam_output_topic').get_parameter_value().string_value
+        model_path = self.get_parameter('model_path').get_parameter_value().string_value
+        input_topic = self.get_parameter('input_topic').get_parameter_value().string_value
+        output_topic = self.get_parameter('output_topic').get_parameter_value().string_value
         queue_size = self.get_parameter('queue_size').get_parameter_value().integer_value
 
    
@@ -49,7 +50,7 @@ class FrontCamDetectorNode(Node):
         )
 
         self.pub_detections = self.create_publisher(
-            VisionObjectArray,
+            Detection2DArray,
             output_topic,
             queue_size
         )
@@ -69,7 +70,9 @@ class FrontCamDetectorNode(Node):
             self.get_logger().error(f"YOLO failed: {e}")
             return
         
-        det_msg = VisionObjectArray()
+        det_msg = Detection2DArray()
+        det_msg.header = msg.header
+        
         det_objects = []
 
         for results in results_list:
@@ -91,18 +94,24 @@ class FrontCamDetectorNode(Node):
                 label = self.class_names[cls_id]
                 cx, cy, w, h = list(box.xywh[0])
 
-                obj = VisionObject()
-                obj.label = label
-                obj.x = float(cx)
-                obj.y = float(cy)
-                obj.z = 0.0
-                obj.theta_z = 0.0
-                obj.extra_field = 0.0
-                obj.confidence = conf
+                detection = Detection2D()
+                detection.header = msg.header
+                
+                # Bounding Box
+                detection.bbox.center.position.x = float(cx)
+                detection.bbox.center.position.y = float(cy)
+                detection.bbox.size_x = float(w)
+                detection.bbox.size_y = float(h)
+                
+                # Hypothesis
+                hypothesis = ObjectHypothesisWithPose()
+                hypothesis.hypothesis.class_id = label
+                hypothesis.hypothesis.score = conf
+                
+                detection.results.append(hypothesis)
+                det_objects.append(detection)
 
-                det_objects.append(obj)
-
-        det_msg.array = det_objects
+        det_msg.detections = det_objects
         self.pub_detections.publish(det_msg)
 
         if det_objects:
