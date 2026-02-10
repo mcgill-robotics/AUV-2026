5d92fb9380a2fbbc554925e05286b26cbe327bd5
avyan-k <avyan.kandasamy@gmail.com>
2026-02-10 04:35:41 +0100

    Add object map node to vision pipeline launch

--- ros2_ws/src/vision/launch/vision_pipeline.launch.py
+++ ros2_ws/src/vision/launch/vision_pipeline.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch_ros.actions import SetParameter
+from launch_ros.actions import SetParameter,Node
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -31,10 +31,22 @@ def generate_launch_description():
         description="Path to the down camera object detection model file."
     )
     
+    # Topic name will be set as constant here for now
+    front_cam_topic = "/zed/zed_node/rgb/color/rect/image"
+    down_cam_topic = "/down_cam/image_raw"
+    front_enhanced_topic = "/vision/front_cam/image_enhanced"
+    down_enhanced_topic = "/vision/down_cam/image_enhanced"
+    front_detections_topic = "/vision/front_cam/detections"
+    down_detections_topic = "/vision/down_cam/detections"
+    
     enhancement_launch = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(os.path.join(vision_dir, "launch", "image_enhancement.launch.xml")),
         # use dictionary unpacking to convert from dict to list of tuples for better readability
         launch_arguments={
+            "front_cam_topic": front_cam_topic,
+            "down_cam_topic": down_cam_topic,
+            "front_enhanced_topic": front_enhanced_topic,
+            "down_enhanced_topic": down_enhanced_topic,
             "sim": LaunchConfiguration("sim"),
             "use_sim_time": LaunchConfiguration("sim"),
         }.items()
@@ -43,11 +55,35 @@ def generate_launch_description():
     object_detection_launch = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(os.path.join(vision_dir, "launch", "object_detection.launch.xml")),
         launch_arguments={
-            "sim": LaunchConfiguration("sim"),
-            "use_sim_time": LaunchConfiguration("sim"),
+            "front_enhanced_topic": front_enhanced_topic,
+            "down_enhanced_topic": down_enhanced_topic,
+            "front_detections_topic": front_detections_topic,
+            "down_detections_topic": down_detections_topic,
             "front_model": LaunchConfiguration("front_model_path"),
             "down_model": LaunchConfiguration("down_model_path"),
+            "use_sim_time": LaunchConfiguration("sim"),
         }.items()
+    )
+    
+    object_map_node = Node (
+        package="vision",
+        executable="object_map_node",
+        name="object_map_node",
+        ros_arguments={
+            "frame_rate": "30",
+            "zed_sdk": "true",
+            "new_object_min_distance_threshold": "0.5",
+            "front_cam_detection_topic": front_detections_topic,
+            "confidence_threshold": "0.5",
+            "max_range": "10.0",
+            "use_stream": "true",
+            "stream_ip": "127.0.0.1",
+            "stream_port": "30000",
+            "show_detections": "true",
+        }.items(),
+        parameters=[
+            {"use_sim_time": LaunchConfiguration("sim")}
+        ]
     )
     return LaunchDescription([
         sim_arg,
@@ -56,4 +92,5 @@ def generate_launch_description():
         SetParameter(name="use_sim_time", value=LaunchConfiguration("sim")),
         enhancement_launch,
         object_detection_launch,
+        object_map_node
     ])
