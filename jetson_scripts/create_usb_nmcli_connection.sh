37e8c7358079124febf5fe3f72bc7876f0464431
avyan-k <avyan.kandasamy@gmail.com>
2026-04-09 20:37:44 +0200

    Remove JP link connection changes from connection script and only modify DVL connection

--- jetson_scripts/create_usb_nmcli_connection.sh
+++ jetson_scripts/create_usb_nmcli_connection.sh
@@ -1,8 +1,4 @@
-
-ROUTER_CONNECTION_NAME="JP-link"
-# static IP assigned by router
-ROUTER_JETSON_IP_ADDRESS=192.168.0.102
-
+#!/bin/bash
 DVL_DEVICE_NAME=enx00e04c2a4978
 # what the DVL thinks the jetson IP address is
 DVL_JETSON_IP_ADDRESS=192.168.194.90
@@ -15,23 +11,18 @@ else
 	echo "Connection '$DVL_CONNECTION_NAME' not found. Creating and configuring"
 	# add USB connection or type ethernet
 	sudo nmcli connection add type ethernet con-name $DVL_CONNECTION_NAME ifname $DVL_DEVICE_NAME ip4 $DVL_JETSON_IP_ADDRESS/$SUBNET_MASK_CIDR
-	
 fi
+
 # make USB connection manual which disables DHCP search
 sudo nmcli connection modify $DVL_CONNECTION_NAME ipv4.method manual
 # never use this connection as default gateway (prevent using it as internet source on boot
 sudo nmcli connection modify $DVL_CONNECTION_NAME ipv4.never-default yes
-
-# prevent router from acting as a gateway to the internet
-sudo nmcli connection modify $ROUTER_CONNECTION_NAME ipv4.never-default yes
-# router provides local network
-sudo nmcli connection modify $ROUTER_CONNECTION_NAME ipv4.ignore-auto-routes yes
-sudo nmcli connection modify $ROUTER_CONNECTION_NAME ipv4.ignore-auto-dns yes
-# assign static IP from router connection
-sudo nmcli connection modify $ROUTER_CONNECTION_NAME ipv4.addresses $ROUTER_JETSON_IP_ADDRESS/$SUBNET_MASK_CIDR
-
-
+sudo nmcli connection modify $DVL_CONNECTION_NAME ipv4.ignore-auto-routes yes
+sudo nmcli connection modify $DVL_CONNECTION_NAME ipv4.ignore-auto-dns yes
+# do not let DVL connection block network-online.target and stall if it is not connected at boot
+sudo nmcli connection modify $DVL_CONNECTION_NAME connection.wait-device-auto-probe no 
+sudo nmcli connection modify $DVL_CONNECTION_NAME connection.autoconnect-retries 1
+sudo nmcli connection modify $DVL_CONNECTION_NAME connection.autoconnect-priority 10
 
 # activate connections
 sudo nmcli connection up $DVL_CONNECTION_NAME
-sudo nmcli connection up $ROUTER_CONNECTION_NAME
