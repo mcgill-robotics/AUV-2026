a005dffd0aaaa2c40a94083b3710feede641793c
Sohaib Kaidali <kaidalisohaib@gmail.com>
2026-04-24 12:09:22 -0400

    Updating model to robosub 2026 detection

--- ros2_ws/src/vision/models/export_model.py
+++ ros2_ws/src/vision/models/export_model.py
@@ -1,4 +1,4 @@
 from rfdetr import RFDETRSmall
 
-model = RFDETRSmall(pretrain_weights="robosub2025-synthetic-rfdetr_s.pth")
+model = RFDETRSmall(pretrain_weights="robosub2026-synthetic-rfdetr_s.pth")
 model.export()
