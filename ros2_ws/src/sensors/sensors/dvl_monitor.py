b7b6ce044be9d19bdeb0ef7001ce50e8147fe5b6
avyan-k <avyan.kandasamy@gmail.com>
2026-05-01 19:35:09 +0200

    Add empty Odom staleness check and empty DVL DR callback

--- ros2_ws/src/sensors/sensors/dvl_monitor.py
+++ ros2_ws/src/sensors/sensors/dvl_monitor.py
@@ -7,17 +7,23 @@ from rclpy.parameter import Parameter
 from diagnostic_msgs.msg import DiagnosticStatus
 from rclpy.time import Time
 
-from dvl_msgs.msg import DVL
+from dvl_msgs.msg import DVL,DVLDR
+from nav_msgs.msg import Odometry
 from sensors.health_monitor import HealthMonitor
 
 class DVLMonitor(HealthMonitor):
     def __init__(self, node):
         super().__init__(node)
+        # Declare DVL-specific parameters
         self.node.declare_parameter("dvl_beam_count",Parameter.Type.INTEGER)
         self.node.declare_parameter("velocity_topic", Parameter.Type.STRING)
-        
+        self.node.declare_parameter("dead_reckoning_topic", Parameter.Type.STRING)
+        self.node.declare_parameter("odometry_topic", Parameter.Type.STRING)
+        # Assign parameters to instance variables
         self.dvl_beam_count = self.node.get_parameter("dvl_beam_count").get_parameter_value().integer_value
-        self.velocity_topic = self.node.get_parameter("velocity_topic").get_parameter_value().string_value
+        velocity_topic = self.node.get_parameter("velocity_topic").get_parameter_value().string_value
+        dead_reckoning_topic = self.node.get_parameter("dead_reckoning_topic").get_parameter_value().string_value
+        odometry_topic = self.node.get_parameter("odometry_topic").get_parameter_value().string_value
         
         # Velocity status
         self.velocity_sub = self.node.create_subscription(
@@ -32,7 +38,27 @@ class DVLMonitor(HealthMonitor):
         self._vel_status_details:dict = {}
         # Add velocity status check to diagnostics updater
         self.assign_status_to_updater("DVL Velocity Status", self._vel_status, self._vel_status_details)
+               
+        self.dr_sub = self.node.create_subscription(
+            DVLDR,
+            dead_reckoning_topic,
+            self.dead_reckoning_callback,
+            self.qos
+        )
+        # overall Status for a given update loop
+        self._dr_status: tuple[bytes, str] = (DiagnosticStatus.STALE, "No dead reckoning data received yet.")
+        # additional details provided as key-value pairs in the diagnostics message
+        self._dr_status_details:dict = {}
+        # Add dead reckoning status check to diagnostics updater
+        self.assign_status_to_updater("DVL Dead Reckoning Status", self._dr_status, self._dr_status_details)
         
+        # odometry sub simply updates the last update time for the stale check in diagnostics, no additional status checks implemented
+        self.odom_sub = self.node.create_subscription(
+            Odometry,
+            odometry_topic,
+            self.odometry_callback,
+            self.qos
+        )
         
         self.last_update_time:Time | None = None
 
@@ -40,7 +66,6 @@ class DVLMonitor(HealthMonitor):
         return self.last_update_time
     
     def velocity_callback(self, msg:DVL) -> None:
-        self.last_update_time = self.node.get_clock().now()
         # Reset detail from previous callback — prevents stale entries persisting
         self._vel_status_details = {}
 
@@ -90,6 +115,12 @@ class DVLMonitor(HealthMonitor):
         self._vel_status_details["Altitude"] = f"{msg.altitude:.3f} m"
         self._vel_status = (DiagnosticStatus.OK, "DVL velocity healthy")
 
+    def dead_reckoning_callback(self, msg:DVLDR) -> None:
+        pass
+    
+    def odometry_callback(self, msg:Odometry) -> None:
+        # Update last update time for diagnostics stale check
+        self.last_update_time = self.node.get_clock().now()
         
         
 def main(args = None):
