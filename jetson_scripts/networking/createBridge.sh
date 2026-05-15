fde1b0bf8107c816d8dcef915cabc4662590f453
avyan-k <avyan.kandasamy@gmail.com>
2026-05-15 03:20:24 +0200

    Merge branch 'dev/dvl' into dev/sensor-health

--- jetson_scripts/networking/createBridge.sh
+++ jetson_scripts/networking/createBridge.sh
@@ -2,8 +2,8 @@
 
 BRIDGE_NAME=br0
 
-JP_LINK=eno1
-DVL_DEVICE=enx00e04c2a4978
+JP_LINK=docker0
+DVL_DEVICE=wlo1
 
 # wait for device names to be available first
 while ! ip link show "$JP_LINK" &>/dev/null || ! ip link show "$DVL_DEVICE" &>/dev/null; do
@@ -16,8 +16,15 @@ if ip link show "$BRIDGE_NAME" &>/dev/null; then
 	sudo ip link set $BRIDGE_NAME down
 	sudo ip link delete $BRIDGE_NAME type bridge
 fi
-
-
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+# warn user about SSH being disabled and ask for confirmation
+echo -e "Warning: This script will create a network bridge between $JP_LINK and $DVL_DEVICE. \n${RED}This will likely disable SSH. ${NC} \nContinue? Y/[n]"
+read tmp
+if [[ "$tmp" != "Y" && "$tmp" != "y" ]]; then
+    echo "Aborting."
+    exit 0
+fi
 sudo ip link add name $BRIDGE_NAME type bridge
 sudo ip link set dev $JP_LINK master $BRIDGE_NAME
 sudo ip link set dev $DVL_DEVICE master $BRIDGE_NAME
